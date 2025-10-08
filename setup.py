2dc9da1cd611585c06ea1ea580fb59cad8aaddb9
Nadim <karim.nadim@nrcan-rncan.gc.ca>
2025-10-08 12:42:56 -0400

    openai connection and prompt created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "FOODIEE Chatbot",
+    version= "0.0.0",
+    author= "Karim Nadim",
+    author_email= "karim_ossama94@hotmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
